ec00d8bac5886282e1ac8573755029532ec82a4b
Alex Cook <apcook7104@gmail.com>
2021-04-12 12:36:50 -0700

    add ec2 configuration schema and unit tests

---
+++ src/ec2_config.py
@@ -0,0 +1,37 @@
+import yaml
+
+class EC2Volume():
+    def __init__(self, device, size_gb, vol_type, mount):
+        self.device = device
+        self.size_gb = size_gb
+        self.vol_type = vol_type
+        self.mount = mount
+
+class EC2User():
+    def __init__(self, login, ssh_key):
+        self.login = login
+        self.ssh_key = ssh_key
+
+class EC2Config():
+    def __init__(self, raw_yaml):
+        ec2_settings = raw_yaml['server']
+
+        self.instance_type = ec2_settings['instance_type']
+        self.ami_type = ec2_settings['ami_type']
+        self.architecture = ec2_settings['architecture']
+        self.root_device_type = ec2_settings['root_device_type']
+        self.virtualization_type = ec2_settings['virtualization_type']
+        self.min_count = ec2_settings['min_count']
+        self.max_count = ec2_settings['max_count']
+
+        volumes = ec2_settings['volumes']
+        if len(volumes) != 2:
+            raise ValueError(f'must supply two volumes for the instance, got {len(volumes)}')
+
+        self.volumes = [EC2Volume(vol['device'], vol['size_gb'], vol['type'], vol['mount']) for vol in volumes]
+
+        users = ec2_settings['users']
+        if len(users) != 2:
+            raise ValueError(f'must supply two users for the instance, got {len(users)}')
+
+        self.users = [EC2User(user['login'], user['ssh_key']) for user in users]
---
+++ tests/test_ec2_config.py
@@ -0,0 +1,65 @@
+import pytest
+import yaml
+
+import ec2_config
+
+def test_file_does_not_exist():
+    """
+    If the EC2 configuration file does not exist, the utility should raise a FileNotFoundError.
+    """
+    
+    with pytest.raises(FileNotFoundError):
+        with open('/some/invalid/path') as f:
+            contents = yaml.safe_load(f.read())
+            config = ec2_config.EC2Config(contents)
+
+def test_file_exists_but_is_not_valid_yaml():
+    """
+    If the given file is not valid YAML, the utility should raise an error.
+    In this particular case, using a str key index on a str type raises a TypeError.
+    """
+
+    with pytest.raises(TypeError):
+        with open('tests/test.txt') as f:
+            contents = yaml.safe_load(f.read())
+            config = ec2_config.EC2Config(contents)
+
+def test_file_with_vaild_yaml():
+    """
+    If the given file is valid YAML, the utility should not raise an error.
+    The instance of the EC2Config class should have all expected properties.
+    """
+
+    with open('tests/test.yaml') as f:
+        contents = yaml.safe_load(f.read())
+        config = ec2_config.EC2Config(contents)
+
+    assert config.instance_type == 't2.micro'
+    assert config.ami_type == 'amzn2'
+    assert config.architecture == 'x86_64'
+    assert config.root_device_type == 'ebs'
+    assert config.virtualization_type == 'hvm'
+    assert config.min_count == 1
+    assert config.max_count == 1
+
+    assert len(config.volumes) == 2
+    for volume in config.volumes:
+        assert isinstance(volume, ec2_config.EC2Volume)
+
+    assert config.volumes[0].device == '/dev/xvda'
+    assert config.volumes[0].size_gb == 10
+    assert config.volumes[0].vol_type == 'ext4'
+    assert config.volumes[0].mount == '/'
+    assert config.volumes[1].device == '/dev/xvdf'
+    assert config.volumes[1].size_gb == 100
+    assert config.volumes[1].vol_type == 'xfs'
+    assert config.volumes[1].mount == '/data'
+
+    assert len(config.users) == 2
+    for user in config.users:
+        assert isinstance(user, ec2_config.EC2User)
+    
+    assert config.users[0].login == 'user1'
+    assert config.users[0].ssh_key == '--user1 ssh public key goes here-- user1@localhost'
+    assert config.users[1].login == 'user2'
+    assert config.users[1].ssh_key == '--user2 ssh public key goes here-- user2@localhost'
