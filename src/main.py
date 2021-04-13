80827305b67ef60a7dfcbb789afec7e793cf7d3b
Alex Cook <apcook7104@gmail.com>
2021-04-12 22:36:43 -0700

    simplify create_instance api call

--- src/main.py
+++ src/main.py
@@ -5,8 +5,6 @@ import cli
 if __name__ == '__main__':
     parser = cli.get_parser()
     args = parser.parse_args()
-    
-    instance_config = ec2_config.EC2Config(args.file)
-    ec2_client = aws.get_client()
 
-    aws.create_instance(ec2_client, instance_config)
+    instance_config = ec2_config.EC2Config(args.file)
+    aws.create_instance(instance_config)
